d1150d29832aa041d5ea71bc532ebbb638a30f0f
Lizzie LaShark & Rahmona Henry <pair+lizzielashark.rahmona-henry@devbootcamp.com>
2016-05-26 18:45:33 +1200

    get and post working, body parser fixed

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ var app = express()
 var bodyParser = require('body-parser')
 var path = require('path')
 var bcrypt = require('bcrypt-node')
+var knex = require('knex')
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
   res.send('success')
+})
+
+app.get('/signup', function (req, res) {
+  res.render('login')
 })
 
 
@@ -23,22 +30,22 @@ app.get('/currentListings', function(req, res){
 
 })
 
-app.get('/signup', function (req, res) {
-  // // if (req.body.email === '') {
-  // //   res.redirect('/sign-up')
-  // // }
-  // var hash = bcrypt.hashSync( req.body.password)
-  // knex('users').insert({ email: req.body.email, hashed_password: hash })
-  // .then(function(data){
-  //   //create sessions
-  //   req.session.userId = data[0]
-    res.render('login',{})
-//   })
-//   .catch(function(error){
-//
-//     req.session.userId = 0
-//     res.redirect('/')
-//   })
+app.post('/signup', function (req, res) {
+//   // if (req.body.email === '') {
+//   //   res.redirect('/sign-up')
+//   // }
+var hash = bcrypt.hashSync( req.body.password)
+ knex('users').insert({ email: req.body.email, hashed_password: hash })
+    .then(function(data){
+        //create sessions
+        // req.session.userId = data[0]
+        res.redirect('currentListings')
+    })
+    .catch(function(error){
+       console.log("error:", error)
+        // req.session.userId = 0
+        res.redirect('/')
+    })
 })
 
 // app.post('/login', function(req, res){

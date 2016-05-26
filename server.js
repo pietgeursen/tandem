56a21eae3b4c721b5accaa22a581aadd4aab0a7b
rath-prak <rathanap@gmail.com>
2016-05-26 20:52:52 +1200

    refactor / server route, WIP main.hbs

--- server.js
+++ server.js
@@ -2,25 +2,38 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var path = require('path')
+var hbs = require('express-hbs')
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 
-app.get('/', function(req, res){
-  res.send('success')
-})
+// setting up landing page
 
+// app.get('/', function(req, res){
+//   res.send('success')
+// })
+
+ app.get('/', function (req, res) {
+    res.render('main', {layout: '_layout'});
+  });
 
 app.get('/currentListings', function(req, res){
-  res.render('currentListings', { }) //handlebars-zie stuff
+  res.render('currentListings', {layout: '_layout'}) //handlebars-zie stuff
   //from = searched
   //to = searched
   // total number of records
   //listing details : date, to, from, driver
 
 })
+
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('catching a lift on 3000!');

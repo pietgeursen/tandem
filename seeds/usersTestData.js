486dee1b9b97eacae30af31695fda7f901f7a980
rath-prak <rathanap@gmail.com>
2016-05-26 21:12:11 +1200

    Update usersTestData.js

--- seeds/usersTestData.js
+++ seeds/usersTestData.js
@@ -5,7 +5,10 @@ exports.seed = function(knex, Promise) {
     knex('users').del(),
 
     // Inserts seed entries
-    knex('users').insert({userID: 1, name: 'Jess', email: 'jess@gmail.com', hashedPassword: 'qwieu838'}),
-    knex('users').insert({userID: 2, name: 'Rahmona', email: 'ra@gmail.com', hashedPassword: 'ewfkjuw'})
+    knex('users').insert({userID: 1, name: 'Buckey', email: 'jess@gmail.com', hashedPassword: 'qwieu838'}),
+    knex('users').insert({userID: 2, name: 'Toni', email: 'ra@gmail.com', hashedPassword: 'ewfkjuw'})
+    knex('users').insert({userID: 3, name: 'Natasha', email: 'ra@gmail.com', hashedPassword: 'ewfkjuw'})
+    knex('users').insert({userID: 4, name: 'Wanda', email: 'ra@gmail.com', hashedPassword: 'ewfkjuw'})
+    knex('users').insert({userID: 5, name: 'Clint', email: 'ra@gmail.com', hashedPassword: 'ewfkjuw'})
   );
 };

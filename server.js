0eef79ea3c379664f6ac8be34d06b77afddaf26b
Maxine Ellah <email": null>
2016-05-26 16:38:25 +1200

    adding packages

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var path = require('path')
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+app.get('/', function(req, res){
+  res.send('success!')
+})
+
+app.listen(3000, function () {
+  console.log('catching a lift on 3000!');
+});

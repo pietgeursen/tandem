22544b908c0fbb5c61d528992a4f15c5992777ae
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-05-27 10:34:35 +1200

    currentListings route renders into _layout.hbs, add code in main.hbs

--- server.js
+++ server.js
@@ -14,22 +14,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-
 app.get('/', function(req, res){
-  res.send('success')
+  res.render('main', { layout: '_layout' })
 })
-
-
 
 app.get('/currentListings', function(req, res){
-
   knex('listings').where({origin: 'Kaeo'}).innerJoin('users', 'listings.userID', '=', 'users.userID')
   .then(function(data){
-  res.render('currentListings', { listing: data })
-
+    res.render('currentListings', { layout: '_layout', listing: data })
   })
 })
-
 
 app.listen(3000, function () {
   console.log('catching a lift on 3000!');

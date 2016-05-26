219fde551441056dc0ad2de38023f56b2006901c
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-05-26 21:17:27 +1200

    create seed data for listings, edit comments seed data

--- seeds/commentsData.js
+++ seeds/commentsData.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     knex('comments').del(),
 
     // Inserts seed entries
-    knex('comments').insert({commentID: 1, listingID: '2', commenterID: 1, comment: 'How flash is your car?'}),
-    knex('comments').insert({commentID: 2, listingID: '3', commenterID: 2, comment: 'How long have you had your licence sucker?'}),
-    knex('comments').insert({commentID: 3, listingID: '5', commenterID: 4, comment: 'Does your car transform into a robot?'})
+    knex('comments').insert({commentID: 1, listingID: 2, commenterID: 1, comment: 'How flash is your car?'}),
+    knex('comments').insert({commentID: 2, listingID: 3, commenterID: 2, comment: 'How long have you had your licence sucker?'}),
+    knex('comments').insert({commentID: 3, listingID: 5, commenterID: 4, comment: 'Does your car transform into a robot?'})
   );
 };

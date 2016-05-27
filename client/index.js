5d7636f87f1c29b2405a4fecc10c21cda84c8d44
Maxine Ellah & Heidi Goosen <pair+maxine-ellah.wishmouse@devbootcamp.com>
2016-05-27 17:45:59 +1200

    search function working

--- client/index.js
+++ client/index.js
@@ -1,15 +1,12 @@
 var request = require('superagent')
 var $ = require('jquery')
+var currentListings = require('../views/_ridesListing.hbs')
 
 console.log("index.js")
 
-function createListingElement(listing){
-  return (
-    '<div>' + listing.origin + '</div>'
-  )
-}
 
 $("#searchButton").click(function(e) {
+  console.log('hiiiiiiiiiiiii!!!')
   e.preventDefault()
   var origin = $("#origin").val()
   var destination = $("#destination").val()
@@ -17,16 +14,8 @@ $("#searchButton").click(function(e) {
     .post('/currentListings')
     .send({ origin: origin, destination: destination})
     .end(function(err, res) {
-      console.log("*******", err, res)
-      var newListing = res.body[0]
-      console.log("newListing: ", createListingElement(newListing))
-      $('.listing').append(createListingElement(newListing))
-
-      // grab the response
-      // grab the element within the page where you want to stick your
-      // updated data
-
-      // append the data within the response, to the element that you have selected
+      var newListing = res.body
+      $('#foo').html(currentListings({ listing: newListing }))
 
     })
 

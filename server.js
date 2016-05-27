5384cefc1fd29c159081e4578f3e9513f5c045ba
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-05-27 20:17:04 +1200

    edit gitignore

--- server.js
+++ server.js
@@ -29,28 +29,28 @@ app.get('/currentListings', function(req, res){
 app.get('/singleListing', function(req, res){
   knex('users').where({'users.userID': 2}).select('*').innerJoin('listings', 'users.userID', 'listings.userID')
   .then(function(data){
-    // console.log('data: ', data)
     res.render('singleListing', { userID: data[0].name, origin: data[0].origin, destination: data[0].destination, date: data[0].dateTime, listingID: data[0].listingID, description: data[0].description, layout: '_layout' })
   })
 })
 
 
 
+// console.log('data: ', data)
+// res.send('success!')
 // { commenterID: req.body.commenterID }
 // commenterID comes from session? params?
 app.post('/singleListing', function(req, res){
   knex('comments').insert({comment: req.body.comment, listingID: req.body.listingID })
   .then(function(data){
-    console.log('yea')
-    console.log('req.body: ', req.body)
-    // res.send('success!')
-    knex.select('comment', 'listingID').from('comments')
-  })
-  .then(function(data){
     console.log('data: ', data)
-    // res.render('listingComment', { layout : 'singleListing' })
-// // will this re-render whole page? with all data from 'get /singleListing' route?
+    // console.log('yea')
+    // console.log('req.body: ', req.body)
+    // knex.select('comment', 'listingID').from('comments')
   })
+//   .then(function(data){
+//     // res.render('listingComment', { layout : 'singleListing' })
+// // // will this re-render whole page? with all data from 'get /singleListing' route?
+//   })
 })
 
 // app.post('/listingComment', function(req, res){

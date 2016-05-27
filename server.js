79ce4894df0e99b728c66b399c6154d368e5f56d
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-05-27 17:46:59 +1200

    WIP comments on listings

--- server.js
+++ server.js
@@ -30,26 +30,34 @@ app.get('/singleListing', function(req, res){
   knex('users').where({'users.userID': 2}).select('*').innerJoin('listings', 'users.userID', 'listings.userID')
   .then(function(data){
     // console.log('data: ', data)
-    res.render('singleListing', { userID: data[0].name, origin: data[0].origin, destination: data[0].destination, date: data[0].dateTime, description: data[0].description, layout: '_layout' })
+    res.render('singleListing', { userID: data[0].name, origin: data[0].origin, destination: data[0].destination, date: data[0].dateTime, listingID: data[0].listingID, description: data[0].description, layout: '_layout' })
   })
 })
 
-// { listingID: req.body.listingID, commenterID: req.body.commenterID, comment: req.body.comment }
-// console.log('req', req.body.comment)
 
+
+// { commenterID: req.body.commenterID }
+// commenterID comes from session? params?
 app.post('/singleListing', function(req, res){
-  knex('comments').insert({comment: req.body.comment})
-  // commenterID comes from session? params?
+  knex('comments').insert({comment: req.body.comment, listingID: req.body.listingID })
+  .then(function(data){
+    console.log('yea')
+    console.log('req.body: ', req.body)
+    // res.send('success!')
+    knex.select('comment', 'listingID').from('comments')
+  })
   .then(function(data){
     console.log('data: ', data)
-    res.send('success!')
-    // knex.select('comment', 'commenterID').from('comments')
+    // res.render('listingComment', { layout : 'singleListing' })
+// // will this re-render whole page? with all data from 'get /singleListing' route?
   })
-//   .then(function(data){
-//     res.render('singleListing', { comment: data.comment, layout: '_layout' })
-// // will this re-render whole page? with all data from get singleListing route?
-//   })
 })
+
+// app.post('/listingComment', function(req, res){
+//
+//
+// })
+
 
 app.listen(3000, function () {
   console.log('catching a lift on 3000!');

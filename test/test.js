7fdb05282e9b2962c32c8cfa11df3abdeeccc1b8
Maxine Ellah & Rahmona Henry <pair+maxine-ellah.rahmona-henry@devbootcamp.com>
2016-05-29 15:00:40 +1200

    second test written and passing

--- test/test.js
+++ test/test.js
@@ -11,3 +11,14 @@ test('visiting homepage takes user to correct view', function(t){
     t.end()
   })
 })
+
+test('posting on homepage redirects to currentListings + origin', function(t) {
+  var origin = { origin: 'Kaeo' }
+  request(app)
+  .post('/currentListings')
+  .send( origin )
+  .end(function(err, res) {
+    t.equal(res.status, 302, 'http status is 302 (redirect)')
+    t.end()
+  })
+} )

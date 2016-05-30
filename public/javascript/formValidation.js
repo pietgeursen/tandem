8fdbb07fa5d3e0d170d15f723ea6f82d0bd65137
Piet Geursen <pietgeursen@gmail.com>
2016-05-30 15:30:30 +1200

    Done

--- public/javascript/formValidation.js
+++ public/javascript/formValidation.js
@@ -1,10 +1,10 @@
 function validateForm() {
   console.log('...boom')
-    var x = document.forms["searchForm"]["origin"].value;
-    if (x == null || x == "") {
-      var message = "Ooops...please enter a start point"
-        document.getElementById("alert").innerHTML = message;
-        return false;
-
-    }
+  var x = document.forms["searchForm"]["origin"].value;
+  //rename x
+  if (x == null || x == "") {
+    var message = "Ooops...please enter a start point"
+    document.getElementById("alert").innerHTML = message;
+    return false;//return might be redundant?
+  }
 }
